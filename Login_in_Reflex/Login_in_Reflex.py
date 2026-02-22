f23af4b4f231991e5e89649921eae1431a3c40e4
TasyDev <tamayocontat@gmail.com>
2026-02-22 18:04:46 -0500

    feat: Init Css and background render

--- Login_in_Reflex/Login_in_Reflex.py
+++ Login_in_Reflex/Login_in_Reflex.py
@@ -1,5 +1,3 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
 import reflex as rx
 
 from rxconfig import config
@@ -8,29 +6,18 @@ from rxconfig import config
 class State(rx.State):
     """The app state."""
 
+def fondo() -> rx.Component:
+    return rx.flex(
+        id="fondo"
+    )
 
-def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
+@rx.page(route="/", title="Login")
+def login() -> rx.Component:
+    return rx.flex (
+
+        fondo(),
+
     )
 
 
-app = rx.App()
-app.add_page(index)
+app = rx.App(stylesheets=["/style.css"])
